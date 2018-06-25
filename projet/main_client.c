cff6e3469279cb76f267a4cef5768415f8c6d792
thimabru1010 <thiago.matheus.bruno@gmail.com>
2018-06-25 12:16:53 +0000

    Client code compiling
    Client code compiling but running with segmentation fault error

--- projet/main_client.c
+++ projet/main_client.c
@@ -5,7 +5,8 @@ int main(void)
 {
 	int status, valread, sock_client;
 	POOL_T sock_col_sensor;
-	struct message message_cs;
+	struct message *message_cs;
+	message_cs = (struct message *) malloc(sizeof(struct message));
 	bool *red_flag;
 
 	//Change this depending on the client
@@ -23,21 +24,22 @@ int main(void)
 	while(true)
 	{
 		// Calculates the PID and makes the car run
-		red_flag = calculate_run_pid(sock_col_sensor);
+		*red_flag = calculate_run_pid(sock_col_sensor);
 
 		// Warn (or not) server if red mark was found (entered on critical section)
 		send( sock_client, (const void *) red_flag, sizeof(red_flag), 0 );
 		
 		// Receive if it's possible to enter on critical section
-		read( sock_client, message_cs, sizeof(message_cs) );
+		read( sock_client, (void *) message_cs, sizeof(message_cs) );
 		
 		// the car will only stop if there is someone on the critcal section
 		//  AND it's not him (check the adress) AND  just if he arrives 
 		//  on red mark, otherwise he can continue to run
-		if( message_cs.flag == false && red_flag == true && strcmp(car_ip, message_cs->car_ip) == 0 )
+		if( message_cs->flag == false && *red_flag == true && strcmp(car_ip, message_cs->car_ip) == 0 )
 			tacho_stop(MOTOR_BOTH);	
 	} 
 
+	free(message_cs);
 	close(sock_client);
 	return 0;
 }
