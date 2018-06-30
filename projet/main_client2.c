d9809fac8b62879f0957bf6aa0a340ce753418b2
thimabru1010 <thiago.matheus.bruno@gmail.com>
2018-06-30 11:56:52 +0000

    Updates
    function calculate_run_pid doesn't works on the while true
    Code of function was put directly on the main function
    PID working properly with tcp connection

--- projet/main_client2.c
+++ projet/main_client2.c
@@ -6,44 +6,51 @@
 
 int main(void)
 {
-	int status, valread, sock_client;
+
+// PID variables --------------------------------------------------------
+
+	float value0, target, error = 0, prop, integral = 0, derivate = 0, PID_value;
+	float last_error = 0, kp = 0.45, ki = 0.0008, kd = 0.9;
+	int duty_cycle, duty_cycle_sp;
 	POOL_T sock_col_sensor;
+// ----------------------------------------------------------------------
+
+// TCP variables -------------------------------------------------------
+	
+	int status, valread, sock_client;
 	struct message *message_cs;
 	message_cs = (struct message *) malloc(sizeof(struct message));
 	strcpy(message_cs->car_ip, "000.000.0.000");
 	bool *red_flag;
 	red_flag = (bool *) malloc(sizeof(bool));
-	
+
+// ---------------------------------------------------------------------
+
 	//Change this depending on the client
 	char car_ip[14] = "192.168.0.104";
 
-	// Initializing the brick
+	// Setting a vlue for the target
+	target = 40;
+
+	// Initialize the brick
 	init_brick();
 
-	// Searching for color sensor
-	sock_col_sensor = search_set_color_sensor();
 
+	// Find and set color sensor
+	sock_col_sensor = search_set_color_sensor();
+   
 	// Making connection with server
-//	sock_client = establish_client_connection(car_ip);
+	sock_client = establish_client_connection(car_ip);
 	
-	float value0, target, error = 0, prop, integral = 0, derivate = 0, PID_value;
-	float last_error = 0, kp = 0.45, ki = 0.0008, kd = 0.9;
-	float left_motor_speed = 0, right_motor_speed = 0;
 	int cont = 0;
-	
-	// Setting a value for target
-	target = 40;
+	float left_motor_speed = 0, right_motor_speed = 0;
 
 	while(true)
 	{
-// ------------------------------------------------------------------
-		// Calculates the PID and makes the car run
-	//	*red_flag = calculate_run_pid(sock_col_sensor);
+//		calculate_run_pid(sock_col_sensor);	
 
+// PID code -------------------------------------------------------------
 		value0 = sensor_get_value0( sock_col_sensor, COLOR_COL_REFLECT);
-
-		if( value0 >= RED_MIN && value0 <= RED_MAX )
-			return true;
 
 		// Error
 		error = target - value0;
@@ -80,10 +87,16 @@ int main(void)
 			printf("Motor left speed: %.2f \t right speed: %.2f \n\n", left_motor_speed, right_motor_speed);
 		}
 
-		cont++;
-	}
-// -------------------------------------------------------------------
-/*
+
+// PID code ending -------------------------------------------------------
+		
+// Transfer data code -----------------------------------------------
+
+		if( value0 >= RED_MIN && value0 <= RED_MAX )
+			*red_flag = true;
+		else
+			*red_flag = false;
+		
 		// Warn (or not) server if red mark was found (entered on critical section)
 		send( sock_client, (const void *) red_flag, sizeof(red_flag), 0 );
 		
@@ -95,11 +108,13 @@ int main(void)
 		//  on red mark, otherwise he can continue to run
 		if( message_cs->flag == false && *red_flag == true && strcmp(car_ip, message_cs->car_ip) == 0 )
 			tacho_stop(MOTOR_BOTH);
-*/
-	 
 
+	
+	}
+	
 	free(red_flag);
 	free(message_cs);
 	close(sock_client);
 	return 0;
+
 }
